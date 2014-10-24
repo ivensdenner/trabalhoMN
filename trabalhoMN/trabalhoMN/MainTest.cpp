3378f4ce0b78ed326c1438f9cd874580a9cfdbdc
ivensdenner <ivensdenner@gmail.com>
2014-10-24 12:48:26 -0300

    Main consertada.

--- trabalhoMN/trabalhoMN/MainTest.cpp
+++ trabalhoMN/trabalhoMN/MainTest.cpp
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
 	Polynomial *polynomial;
 	int degree;
 
-	std::cout << "Digite o grau do polinomio: ";
-	std::cin >> degree;
+//	std::cout << "Digite o grau do polinomio: ";
+//	std::cin >> degree;
 	degree = 4;
 
 	polynomial = new Polynomial(degree);
@@ -23,9 +23,8 @@ int main(int argc, char **argv)
 		polynomial->setCoefficient(coef, i);
 	}
 
-	double root = newtonRaphson(polynomial, 2, 0.0001, 0.0001, 50);
+	double root = newtonRaphson(polynomial, 0.6, 0.0001, 0.0001, 50);
 	std::cout << "Raiz: " << root << std::endl;
-	//std::cout << "Resultado: " << polynomial->derivadaNumerica(2) << std::endl;
 
 	system("pause");
 
